71ed1cd74adc2ebd9bd4d345a51d5ad077c8feff
Robin Mennel <robinmennel88@gmail.com>
2024-03-17 21:52:49 -0500

    passing tests

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,19 @@
+import satData from "./satData";
+import './styling.css'; 
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className="flex-container">
+      <div>
+        {displaySats.map((sat, id) => {
+          return (
+            <button onClick={() => filterByType(sat)} key={id}>
+              {sat} Orbit
+            </button>
+          );
+        })}
+      </div>
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
